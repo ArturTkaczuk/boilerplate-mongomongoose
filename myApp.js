7b4b35a5891307946c446584e87b43805a36110b
Artur Tkaczuk <tkaczukartur@gmail.com>
2022-08-25 17:44:33 +0200

    Create Many Records with model.create()

--- myApp.js
+++ myApp.js
@@ -36,7 +36,13 @@ const createAndSavePerson = (done) => {
 };
 
 const createManyPeople = (arrayOfPeople, done) => {
-  done(null /*, data*/);
+  Person.create(arrayOfPeople, (err, data) => {
+    if (err) {
+      console.error(err);
+    }
+
+    done(null, data);
+  });
 };
 
 const findPeopleByName = (personName, done) => {
