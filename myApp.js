d54e4db2644c441175870e8f90ced12a975eeae7
Artur Tkaczuk <tkaczukartur@gmail.com>
2022-08-25 17:21:58 +0200

    Create and Save a Record of a Model

--- myApp.js
+++ myApp.js
@@ -20,6 +20,7 @@ const personSchema = new mongoose.Schema({
 const Person = mongoose.model("Person", personSchema);
 
 const createAndSavePerson = (done) => {
+  Person.save((err, data) => {});
   done(null /*, data*/);
 };
 
